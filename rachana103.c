87843c66907892e1a818a66eae24c69c0e133f58
Rachana240906 <u24ai064@aid.svnit.ac.in>
2024-11-28 14:32:57 +0530

    Create rachana103.c

---
+++ rachana103.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+int main() 
+{
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("The bigger number is: %d\n", MAX(num1, num2));
+    return 0;
+}
